cf3dea9d54c30a3bfc80513aef44abe226b2285d
Kestler Barrios <luiseduardo3213@gmail.com>
2020-07-05 02:43:09 -0600

    Register y Login Funcional

--- src/controller/Controllers.js
+++ src/controller/Controllers.js
@@ -4,38 +4,58 @@ const bcrypt = require('bcrypt-nodejs')
 const jwt = require('../services/jwt')
 
 const User = require('../models/User')
+var arrayCommands = []
 
-exports.commands = function () {
-    var option = String
-    switch (option) {
-        case register:
-            exports.REGISTER
+exports.commands = function (req, res) {
+    var params = req.body
+    arrayCommands = params.commands.split(" ")
+    switch (arrayCommands[0]) {
+        case "register":
+            REGISTER(req, res)
             break;
-
+        case "login":
+            LOGIN(req,res)
+            break;
+        case "edituser":
+            editUser(req,res)
+            break;
+        case "deleteuser":
+            deleteUser(req,res)
+            break;
+        case "profile":
+            profile(req,res)
+            break;
+        case "follow":
+            FOLLOW(req,res)
+            break;
+        case "unfollow":
+            UNFOLLOW(req,res)
+            break;
         default:
+            res.status(404).send({message: "Codigo invalido"})
             break;
     }
 }
 
-exports.REGISTER = function (req, res) {
+function REGISTER(req, res) {
+    var username = arrayCommands[1]
+    var password = arrayCommands[3]
     var user = new User()
-    var params = req.body
-
-    if (params.userName && params.password) {
-        user.userName = params.userName
-        User.find({ userName: user.userName }).exec((err, users) => {
+    if (username && password) {
+        user.userName = username
+        User.find({ userName: username }).exec((err, users) => {
             if (err) return res.status(500).send({ message: 'Error en la peticion.' })
             if (users && users.length >= 1) {
                 return res.status(500).send({ message: 'El Usuario ya existe.' })
             } else {
-                bcrypt.hash(params.password, null, null, (err, hash) => {
+                bcrypt.hash(password, null, null, (err, hash) => {
                     user.password = hash
                     user.save((err, userSave) => {
                         if (err) return res.status(500).send({ message: 'Error al guardar el Usuario.' })
                         if (userSave) {
                             res.status(200).send({ usuario: userSave })
                         } else {
-                            res.status(404).send({ message: `No se ha podido registrar ${params.nombre} ` })
+                            res.status(404).send({ message: `No se ha podido registrar ${username} ` })
                         }
                     })
                 })
@@ -46,18 +66,19 @@ exports.REGISTER = function (req, res) {
 
 }
 
-exports.LOGIN = function (req, res) {
-    const params = req.body
-    User.findOne({ userName: params.userName }, (err, user) => {
+function LOGIN(req, res) {
+    var username = arrayCommands[1]
+    var password = arrayCommands[2]
+    User.findOne({ userName: username }, (err, user) => {
         if (err) return res.status(500).send({ message: 'Error en la peticion' })
         if (user) {
-            bcrypt.compare(params.password, user.password, (err, check) => {
+            bcrypt.compare(password, user.password, (err, check) => {
                 if (check) {
-                    if (params.gettoken) {
+                    if (arrayCommands[3]) {
                         return res.status(200).send({ token: jwt.createToken(user) })
                     } else {
-                        admin.password = undefined
-                        return res.status(200).send({ hotel: user })
+                        user.password = undefined
+                        return res.status(200).send({ user : user })
                     }
                 } else {
                     res.status(404).send({ message: 'El usuario no se ha podido identificar.' })
@@ -69,7 +90,7 @@ exports.LOGIN = function (req, res) {
     })
 }
 
-exports.editUser = function (req, res) {
+function editUser(req, res) {
     const userId = req.params.id
     const params = req.body
 
@@ -85,7 +106,7 @@ exports.editUser = function (req, res) {
 
 }
 
-exports.deleteUser = function (req, res) {
+function deleteUser(req, res) {
     const userId = req.params.id
     if (userId != req.user.sub) {
         return res.status(500).send({ message: 'No posee los permisos para eliminar el Usuario' })
@@ -97,7 +118,7 @@ exports.deleteUser = function (req, res) {
     })
 }
 
-exports.PROFILE = function (req, res) {
+function PROFILE(req, res) {
     var params = req.body
     User.findOne({ userName: params.username }).exec((err, profile) => {
         if (err) return res.status(500).send({ message: 'Error en la peticion' })
@@ -105,10 +126,10 @@ exports.PROFILE = function (req, res) {
     })
 }
 
-exports.FOLLOW = function (username) {
+function FOLLOW(username) {
 
 }
 
-exports.UNFOLLOW = function (username) {
+function UNFOLLOW(username) {
 
 }
