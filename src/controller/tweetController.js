c741c484aba6985c2b1e0a203dc51b7497f20c70
Kestler Barrios <luiseduardo3213@gmail.com>
2020-08-16 19:16:50 -0600

    Editar Tweet funcionando

--- src/controller/tweetController.js
+++ src/controller/tweetController.js
@@ -40,14 +40,13 @@ exports.DELETE_TWEET = function (req, res, arrayC) {
     Tweet.findOneAndDelete({ userName: userName, _id: tweetId }, (err, tweetDeleted) => {
         if (err) return res.status(500).send({ message: 'Error en la peticion' })
         if (!tweetDeleted) return res.status(404).send({ message: 'Error al guardar el Tweet' })
-        // return res.status(200).send({ message: 'Tweet eliminado', tweetEliminado: tweetDeleted })
-        if(tweetDeleted){
+        if (tweetDeleted) {
             User.findOneAndUpdate({ userName: userName }, { $inc: { "tweets": -1 } }, { new: true }, (err, userUpdated) => {
                 if (err) return res.status(500).send({ message: 'Error en la peticion' })
                 if (!userUpdated) return res.status(404).send({ message: 'Error al sumar el Tweet' })
                 if (userUpdated) return res.status(200).send({ message: 'Tweet eliminado', tweetEliminado: tweetDeleted })
             })
-            
+
         }
     })
 
@@ -55,22 +54,26 @@ exports.DELETE_TWEET = function (req, res, arrayC) {
 }
 
 exports.EDIT_TWEET = function (req, res, arrayC) {
-    const tweetId = arrayC[1]
+    var userName = req.user.userName
+    var tweetId = arrayC[1]
     arrayC.shift()
     arrayC.shift()
     var newTweet = arrayC.join(" ")
+    if (newTweet) {
+        Tweet.findByIdAndUpdate({ usernName: userName, _id: tweetId }, { text: newTweet }, { new: true }, (err, tweetUpdated) => {
+            if (err) return res.status(500).send({ message: 'Error en la peticion' })
+            if (!tweetUpdated) return res.status(404).send({ message: 'No se ha podido editar el Tweet' })
+            return res.status(200).send({ tweetNuevo: tweetUpdated })
+        })
 
-    if (tweetId != req.user.sub) {
-        return res.status(500).send({ message: 'No posee los permisos para Editar el Tweet' })
     }
-    Tweet.findByIdAndUpdate(tweetId, newTweet, { new: true }, (err, tweetUpdated) => {
-        if (err) return res.status(500).send({ message: 'Error en la peticion' })
-        if (!tweetUpdated) return res.status(404).send({ message: 'No se ha podido editar el Tweet' })
-        return res.status(200).send({ tweet: tweetUpdated })
-
-    })
 }
 
 exports.VIEW_TWEETS = function (req, res, arrayC) {
+    var userName = arrayC[1]
 
+    if (userName) {
+        Tweet.find({ userName: userName }, (err, tweets) => { })
+
+    }
 }
